cf0e78c3c4d00c7eedaf5d6d08081afb0a32f3e8
Joseph19820124 <Neal_Fullerxcz@physicist.net>
2026-05-11 07:50:44 +0000

    chore: add .gitignore for bin/obj/node_modules

--- infra/cdk/dist/lib/ecommerce-stack.d.ts
+++
@@ -1,9 +0,0 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import { EnvConfig } from './config';
-export interface ECommerceStackProps extends cdk.StackProps {
-    config: EnvConfig;
-}
-export declare class ECommerceStack extends cdk.Stack {
-    constructor(scope: Construct, id: string, props: ECommerceStackProps);
-}
